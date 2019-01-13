05d41939cb04ba2c62bda86719222c0eb6791ff8
aleeraser <alezini94@hotmail.it>
2019-01-13 19:19:20 +0100

    Updated switch button image and code.

--- lib/customImageButton.dart
+++ lib/customImageButton.dart
@@ -9,42 +9,45 @@ class CustomImageButton extends StatelessWidget {
 
   CustomImageButton({@required this.assetImage, @required this.onTap});
 
+  // TODO: the button is rectangular shaped even if the image is circular. Make the 'tappable-area' circular too.
+
   @override
   Widget build(BuildContext context) {
-
-    return Container(
-      child: ConstrainedBox(
-        constraints: BoxConstraints.tightFor(height: 300),
-        child: Ink.image(
-          image: assetImage,
-          fit: BoxFit.fitWidth,
-          child: InkWell(
-            highlightColor: Colors.transparent,
-            splashColor: Colors.transparent,
-            onTap: onTap,
-          ),
-        ),
-      ),
-    );
-
-    // new FloatingActionButton(
-    //   isExtended: true,
-    //   onPressed: () {
-    //     setState(() {
-    //       _status = !_status;
-    //       if (_status) {
-    //         _image = new Image(image: AssetImage("assets/images/btn_on.png"));
-    //         _bgColor = Colors.white;
-    //       } else {
-    //         _image = new Image(image: AssetImage("assets/images/btn_off.png"));
-    //         _bgColor = new Color.fromARGB(255, 49, 58, 73);
-    //       }
-    //     });
-    //   },
-    //   child: new ConstrainedBox(
-    //     constraints: new BoxConstraints.expand(),
-    //     child: _image,
+    // return Container(
+    //   child: ConstrainedBox(
+    //     constraints: BoxConstraints.tightFor(height: 300),
+    //     child: Ink.image(
+    //       // padding: EdgeInsets.symmetric(horizontal: 60.0),
+    //       image: assetImage,
+    //       fit: BoxFit.fitWidth,
+    //       child: InkWell(
+    //         highlightColor: Colors.transparent,
+    //         splashColor: Colors.transparent,
+    //         onTap: onTap,
+    //       ),
+    //     ),
     //   ),
-    // ),
+    // );
+
+    // Using a FloatingActionButton since it is naturally a circle.
+    return Container(
+        width: double.infinity,
+        child: FittedBox(
+            child: FloatingActionButton(
+                onPressed: null, // tap is handled below
+                backgroundColor: Colors.transparent,
+                elevation: 0, // grounded
+                child: Container(
+                  child: Ink.image(
+                    // padding: EdgeInsets.symmetric(horizontal: 60.0),
+                    image: assetImage,
+                    fit: BoxFit.fitWidth,
+                    child: InkWell(
+                      highlightColor: Colors.transparent,
+                      splashColor: Colors.transparent,
+                      onTap: onTap,
+                    ),
+                  ),
+                ))));
   }
 }

a35972d97bfea51c82881d7a44c8f81705e4fc25
aleeraser <alezini94@hotmail.it>
2019-01-13 19:38:46 +0100

    Rewritten startup and socket connection logic. Socket timeout handling.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'settingsView.dart';
+
 import 'customImageButton.dart';
+import 'settingsView.dart';
+
+const SOCKET_TIMEOUT = 5;
+
+Socket s;
+
+enum Commands { ATON, ATOFF, ATPRINT, ATZERO, ATRESET, ATPOWER, ATREAD, ATSTATE }
 
 void main() => runApp(MyApp());
 
@@ -12,22 +21,28 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Color.fromARGB(255, 0, 51, 50),
       ),
-      home: SocketHomePage(),
+      home: SmartSocketHomePage(),
     );
   }
 }
 
-class SocketHomePage extends StatefulWidget {
+class SmartSocketHomePage extends StatefulWidget {
   @override
-  _SocketHomePageState createState() => _SocketHomePageState();
+  _SmartSocketHomePageState createState() => _SmartSocketHomePageState();
 }
 
-class _SocketHomePageState extends State<SocketHomePage> {
-  // String _socketStatus = 'Socket is off';
-  bool _status = false; 
-  var _assetImage = AssetImage("assets/images/btn_off.png");
-  var _image = new Image(image: AssetImage("assets/images/btn_off.png"));
-  var _bgColor = new Color.fromARGB(255, 49, 58, 73);
+class _SmartSocketHomePageState extends State<SmartSocketHomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  bool _status;
+  String get statusText {
+    if (_status == null) {
+      return 'Loading...';
+    } else if (_status) {
+      return 'Socket is on';
+    } else {
+      return 'Socket is off';
+    }
+  }
 
   void _navigateToSettings() {
     Navigator.of(context).push(
@@ -39,11 +54,73 @@ class _SocketHomePageState extends State<SocketHomePage> {
     );
   }
 
+  void _destroySocket() {
+    if (s != null) {
+      s.close();
+      s.destroy();
+      s = null;
+      // debugPrint("Destroyed socket.");
+    } else {
+      debugPrint("Socket wasn't opened.");
+    }
+  }
+
+  void _socketConnection({Commands command, String url, int port, Function onDataCallback, Function onErrorCallback, Function onDoneCallback}) {
+    final _url = url != null ? url : '192.168.4.1';
+    final _port = port != null ? port : 8888;
+    final _commandStr = command.toString().replaceAll('Commands.', '');
+
+    Socket.connect(_url, _port, timeout: Duration(seconds: SOCKET_TIMEOUT)).then((Socket _s) {
+      s = _s;
+      s.write('$_commandStr\n');
+      s.listen(onDataCallback != null ? onDataCallback : null, onError: onErrorCallback != null ? onErrorCallback : (error) => _showMessage('Error'), cancelOnError: true,
+          onDone: () {
+        _destroySocket();
+
+        if (onDoneCallback != null) onDoneCallback();
+        debugPrint('$_commandStr completed');
+      });
+    }).catchError((error) {
+      final e = error as SocketException;
+      debugPrint(e.message);
+
+      // FIXME: is there really no other way?
+      if (e.message.toLowerCase().contains('timed out')) {
+        _showMessage('Error: timeout.', error: true);
+      } else {
+        _showMessage('Error.', error: true);
+      }
+    });
+  }
+
+  void _updateStatus({Function onDoneCallback}) {
+    _socketConnection(command: Commands.ATSTATE, onDataCallback: (data) => _status = String.fromCharCodes(data) == '1', onDoneCallback: onDoneCallback);
+  }
+
+  void _showMessage(String msg, {bool error = false}) {
+    _scaffoldKey.currentState.showSnackBar(SnackBar(
+      content: Text(msg),
+      backgroundColor: error ? Colors.red[900] : null,
+    ));
+  }
+
+  @override
+  void initState() {
+    if (_status == null) {
+      _updateStatus(onDoneCallback: () {
+        debugPrint('Status: ${_status ? 'on' : 'off'}');
+        setState(() => null);
+      });
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        title: Text('Smart Socket'),
+        title: const Text('Smart Socket'),
         centerTitle: true,
         actions: <Widget>[
           IconButton(
@@ -52,29 +129,48 @@ class _SocketHomePageState extends State<SocketHomePage> {
           ),
         ],
       ),
-      body: new Center(
+      body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
             CustomImageButton(
-                assetImage: _assetImage,
+                assetImage: _status != null && _status ? const AssetImage('assets/images/btn_on.png') : const AssetImage('assets/images/btn_off.png'),
                 onTap: () {
-                  setState(() {
-                    _status = !_status;
-                    if (_status) {
-                      _assetImage = new AssetImage("assets/images/btn_on.png");
-                      _bgColor = Colors.white;
-                    } else {
-                      _assetImage = new AssetImage("assets/images/btn_off.png");
-                      _bgColor = new Color.fromARGB(255, 49, 58, 73);
-                    }
-                  });
+                  _socketConnection(
+                      command: _status ? Commands.ATOFF : Commands.ATON,
+                      onDoneCallback: () {
+                        final _oldStatus = _status;
+
+                        _updateStatus(
+                            onDoneCallback: () => setState(() {
+                                  if (_oldStatus == _status) {
+                                    _showMessage('Error');
+                                  }
+                                }));
+                      });
                 }),
-            Text("data")
+            Text(statusText),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                IconButton(
+                  icon: const Icon(Icons.adb),
+                  onPressed: () {
+                    _updateStatus();
+                  },
+                ),
+                IconButton(
+                  icon: const Icon(Icons.cancel),
+                  onPressed: () {
+                    _destroySocket();
+                  },
+                )
+              ],
+            ),
           ],
         ),
       ),
-      backgroundColor: _bgColor,
+      backgroundColor: _status != null && _status ? Colors.white : const Color.fromARGB(255, 49, 58, 73),
     );
   }
 }

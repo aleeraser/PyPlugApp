e389024f32fe216ae96a4589dbd4c42a331dcdee
aleeraser <alezini94@hotmail.it>
2019-01-24 12:51:37 +0100

    Added Common.

---
+++ lib/Common.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const COLOR_ON = Color.fromARGB(255, 255, 255, 255);
+const COLOR_OFF = Color.fromARGB(255, 49, 58, 73);
+
+enum Status { ON, OFF, UNKNOWN, LOADING }
+
+const DEFAULT_REFRESH_INTERVAL = 10;

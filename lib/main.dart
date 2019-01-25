431fcee31ccd4263430466db84a1cb224e9a6f9b
aleeraser <alezini94@hotmail.it>
2019-01-25 19:42:12 +0100

    Added orientation lock and some other debug comments.

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'Common.dart';
 import 'DevicesListView.dart';
+import 'DeviceDetailsView.dart';
+import 'SettingsView.dart';
 import 'PersistanceHandler.dart';
 
 main() {
-  PersistanceHandler.getHandler().init();
-  // ..whenComplete(() {
-  //   debugPrint(PersistanceHandler.getHandler().getKeys().toString());
-  // });
-  runApp(MyApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).whenComplete(() {
+    PersistanceHandler.getHandler().init().whenComplete(() {
+      // debugPrint(PersistanceHandler.getHandler().getKeys().toString());
+
+      // PersistanceHandler.getHandler().clear();
+      // PersistanceHandler.getHandler().setForDevice('30:ae:a4:bf:39:68', 'address', '192.168.1.8');
+      // PersistanceHandler.getHandler().setForDevice('30:ae:a4:bf:39:68', 'port', '8888');
+      // PersistanceHandler.getHandler().setForDevice('30:ae:a4:bf:39:68', 'device_name', 'Hell yeah');
+      // PersistanceHandler.getHandler().setForDevice('30:ae:a4:bf:39:68', 'refresh_interval', '0');
+      // PersistanceHandler.getHandler().setForDevice('30:ae:a4:bf:39:68', 'ssid', 'HoLaCaccaWiFi');
+      // PersistanceHandler.getHandler().setForDevice('30:ae:a4:bf:39:68', 'password', 'ciaociao');
+
+      runApp(MyApp());
+    });
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -21,7 +34,13 @@ class MyApp extends StatelessWidget {
         primaryColor: COLOR_OFF,
       ),
       home: DevicesListView(),
-      // home: Text('CACCA'),
+      // home: DeviceDetailsView(
+      //   deviceID: '30:ae:a4:bf:39:68',
+      // ),
+      // home: SettingsView(
+      //   deviceID: '30:ae:a4:bf:39:68',
+      //   prevPrefValues: {'address': '192.168.1.8', 'port': '8888', 'device_name': 'Hell yeah', 'refresh_interval': '0', 'ssid': 'HoLaCaccaWiFi', 'password': 'ciaociao'},
+      // ),
     );
   }
 }
